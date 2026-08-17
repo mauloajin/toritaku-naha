b93713ddff059653a71f191cd871c29f1ab75559
mauloajin <mauloa32153@gmail.com>
2026-08-17 11:37:50 +0900

    Add Search Console verification

--- app/sitemap.js
+++ app/sitemap.js
@@ -2,7 +2,7 @@ export default function sitemap() {
   return [
     {
       url: "https://toritaku-naha.vercel.app/",
-      lastModified: new Date("2026-08-11"),
+      lastModified: new Date("2026-08-17"),
       changeFrequency: "weekly",
       priority: 1,
     },
